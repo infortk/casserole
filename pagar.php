19b4f500a8dda83a10bdc376ef7370b41eb688af
infortk <75238287+infortk@users.noreply.github.com>
2020-11-30 01:09:48 -0500

    agrego mi primera pagina web

---
+++ pagar.php
@@ -0,0 +1,8 @@
+<?php
+$banco = $_POST["banco"];
+$precio = $_POST["precio"];
+$tipodeproducto =$_POST["tipodeproducto"];
+
+echo "<p> $banco</p>";
+echo "<p> $precio</p>";
+echo "<p> $tipodeproducto</p>";
